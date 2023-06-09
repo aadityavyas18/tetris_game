a5dae8db7b69be3dc0bbb2879578f5bdeb115103
aadityavyas18 <94797769+aadityavyas18@users.noreply.github.com>
2023-06-09 08:15:23 +0530

    Update Init.kt

--- src/commonMain/kotlin/ds/tetris/di/Init.kt
+++ src/commonMain/kotlin/ds/tetris/di/Init.kt
@@ -1,7 +1,8 @@
 package ds.tetris.di
 
 import org.koin.core.context.startKoin
-
+//The koin works on simple DSL model
+//all the dependent objects are there
 fun initKoin(){
     startKoin {
         modules(gameModule, platformModule)
